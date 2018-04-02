5429460a20184172e88a56310c98cc9dfc840544
xuziang111 <584788096@qq.com>
2018-04-02 16:18:56 +0800

    ajax

--- main.js
+++ main.js
@@ -35,5 +35,23 @@ let dom={
             object2[key] = clone(object[key])
         }
         return object2
+    },
+    ajax:function(url,method,success,fail,body,header){
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status >= 200 && request.status < 300 ){
+                    success(request.responseText,request.status)
+                }else{
+                    fail(request.responseText,request.status)
+                }
+            }
+        }
+        if(header){
+            for(let i in header)
+            request.setRequestHeader(i,header[i])
+        }
+        request.send(body)
     }
 }
